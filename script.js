d2ed6309c8158d034d80120bf5736df3a9420e84
Vasyausach <32095634+Vasyausach@users.noreply.github.com>
2019-03-03 14:02:40 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+'use strict';
+
+let money, time, d, op;
+
+money = +prompt('Ваш бюджет на месяц','');
+time = prompt('Введите дату в формате YYYY-MM-DD','');
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    savings: false,
+    income: []
+};
+
+console.log(appData.expenses);
+
+for (let i = 0;i < 2; i++) {
+    let price = prompt("Введите обязательную статью расходов в этом месяце", ""),
+        cost = +prompt("Во сколько обойдется?", "");
+
+    if ( (typeof(price)) === 'string' && (typeof(price)) !=null && (typeof(cost) !=null
+        && price !='' && cost !='' && price.length < 50)){
+    console.log("done");
+    appData.expenses[price] = cost;
+    } else{
+        i--;
+    }
+};
+
+appData.moneyPerday = appData.budget / 30;
+
+if (appData.moneyPerday <1000) {
+    console.log('Низкий уровень достатка');
+} else if (appData.moneyPerday >1000 && appData.moneyPerday < 2000) {
+    console.log('Средний уровень достатка');
+} else if (appData.moneyPerday > 2000) {
+    console.log('Высокий уровень достатка');
+} else {
+    console.log('Ой, ошибочка!');
+}
+
+alert('Ежедневный бюджет' + appData.moneyPerday);
