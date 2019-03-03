051565890781d37156adef78e504c6d5cb398209
Vasyausach <32095634+Vasyausach@users.noreply.github.com>
2019-03-03 15:06:24 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -29,6 +29,40 @@ for (let i = 0;i < 2; i++) {
     }
 };
 
+
+
+// let i=0;
+// while(i<2){
+// let price = prompt("Введите обязательную статью расходов в этом месяце", ""),
+//     cost = +prompt("Во сколько обойдется?", "");
+//     if ( (typeof(price)) === 'string' && (typeof(price)) !=null && (typeof(cost) !=null
+//         && price !='' && cost !='' && price.length < 50)){
+//     console.log("done");
+//     appData.expenses[price] = cost;
+//     } else{
+//         i--;
+//      }
+//     i++;
+// };
+
+
+
+// let i=0;
+// do {
+//     let price = prompt("Введите обязательную статью расходов в этом месяце", ""),
+//      cost = +prompt("Во сколько обойдется?", "");
+//      if ( (typeof(price)) === 'string' && (typeof(price)) !=null && (typeof(cost) !=null
+//          && price !='' && cost !='' && price.length < 50)){
+//      console.log("done");
+//      appData.expenses[price] = cost;
+//      } else{
+//          i--;
+//       }
+//      i++;
+// } while(i<2);
+
+
+
 appData.moneyPerday = appData.budget / 30;
 
 if (appData.moneyPerday <1000) {
